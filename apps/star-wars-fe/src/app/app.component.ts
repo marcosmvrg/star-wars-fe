8c5ca83d28024f4036971ba6976965be6a7241d2
Marcos Gomes <marcosmvrg@gmail.com>
2023-11-01 14:53:24 +0000

    Initial commit

---
+++ apps/star-wars-fe/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'star-wars-fe-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'star-wars-fe';
+}
